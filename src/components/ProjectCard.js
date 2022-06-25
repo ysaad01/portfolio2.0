9b198d380994830178d2770e6fafd4b2134de8e3
ysaad01 <ysaad2325@gmail.com>
2022-06-25 19:18:16 -0400

    completed Resume.js page

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,20 +4,8 @@ import { Card, Button, Col } from "react-bootstrap";
 const ProjectCard = ({ project }) => {
   console.log(project);
   return (
-    // <Card style={{ width: "18rem" }}>
-    //   <Card.Img variant="top" src="holder.js/100px180" />
-    //   <Card.Body>
-    //     <Card.Title>Card Title</Card.Title>
-    //     <Card.Text>
-    //       Some quick example text to build on the card title and make up the
-    //       bulk of the card's content.
-    //     </Card.Text>
-    //     <Button variant="primary">Go somewhere</Button>
-    //   </Card.Body>
-    // </Card>
-
     <Col xs={12} md={6} lg={4} xl={3} className="d-flex justify-content-center">
-      <Card style={{ width: "18rem" }}>
+      <Card style={{ width: "22rem" }}>
         <Card.Img
           variant="top"
           src={`${process.env.PUBLIC_URL}/images/${project.image}`}
--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,22 +1,54 @@
 import React from "react";
-import { Row } from "react-bootstrap";
-import { Nav, Button } from "react-bootstrap";
+import { Nav, Button, Row, Col } from "react-bootstrap";
 
 const Resume = () => {
   return (
     <div className="page-content">
       <h1>Resume</h1>
+      <Nav className="resume-pdf">
+        <Button
+          variant="link"
+          href={`${process.env.PUBLIC_URL}/yas-resume.pdf`}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Full Resume
+        </Button>
+      </Nav>
+
       <Row>
-        <Nav className="resume-pdf">
-          <Button
-            variant="link"
-            href={`${process.env.PUBLIC_URL}/yas-resume.pdf`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            Full Resume
-          </Button>
-        </Nav>
+        <Col>
+          <h3> Education </h3>
+          <ul>
+            <li>Michigan State University, Full Stack Bootcamp</li>
+            <li>Baldwin Wallace University, International Studies</li>
+          </ul>
+          <br />
+          <h3>Work History</h3>
+          <h4>Rocket Mortgage</h4>
+          <ul>
+            <li>SOS Loan Specialist</li>
+            <li>Power Banker</li>
+          </ul>
+          <br />
+          <h4>Angel Dental Care</h4>
+          <ul>
+            <li>Office Receptionist</li>
+          </ul>
+        </Col>
+
+        <Col>
+          <h3>Technology Skills</h3>
+          <br />
+          <h4>Experience With:</h4>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>Github</li>
+            <li>ETC</li>
+            <li>ETC</li>
+          </ul>
+        </Col>
       </Row>
     </div>
   );

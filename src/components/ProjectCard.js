9e365b12984fe4f1485a3cfbcb6e59e61e382471
ysaad01 <ysaad2325@gmail.com>
2022-06-26 14:41:10 -0400

    fixed project card layout to show 3 cards per row on xl screen

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,8 +4,8 @@ import { Card, Button, Col } from "react-bootstrap";
 const ProjectCard = ({ project }) => {
   console.log(project);
   return (
-    <Col xs={12} md={3} lg={4} xl={3} className="d-flex justify-content-center">
-      <Card style={{ width: "23rem" }}>
+    <Col xs={12} md={6} lg={4} xl={4} className="d-flex justify-content-center">
+      <Card style={{ width: "25rem" }}>
         <Card.Img
           variant="top"
           src={`${process.env.PUBLIC_URL}/images/${project.image}`}
